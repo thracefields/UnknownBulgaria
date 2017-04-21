6ee97c2851c594f1bba62cb085893aacc9bb1f52
thracefields <thracefields@gmail.com>
2017-04-21 19:28:28 +0300

    Initial Commit

---
+++ config/config.php
@@ -0,0 +1,6 @@
+const DATABASE_HOST = 'locahost';
+const DATABASE_USERNAME = 'root';
+const DATABASE_PASSWORD = '';
+const DATABASE = '';
+
+$connect = mysqli_connect(DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE);
